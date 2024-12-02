02b2e3c0cc57d697af4fdd68cd9dc06f92099022
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-12-02 00:29:07 +0000

    prettify day 1

--- typescript/day1.ts
+++ typescript/day1.ts
@@ -1,27 +1,64 @@
 import { readFileSync } from "fs";
 
-const data = readFileSync("./input/day1.txt").toString();
+// Setup
+function setup() {
+	const data = readFileSync("./input/day1.txt").toString();
 
-const lines = data.split("\n");
-const firstList: number[] = [];
-const secondList: number[] = [];
+	const lines = data.split("\n");
 
-for (const line of lines) {
-	const [first, second] = line.split("   ");
-	firstList.push(parseInt(first));
-	secondList.push(parseInt(second));
+	const leftList: number[] = [];
+	const rightList: number[] = [];
+
+	for (const line of lines) {
+		const [left, right] = line.split("   ");
+		leftList.push(parseInt(left));
+		rightList.push(parseInt(right));
+	}
+
+	return { leftList, rightList };
 }
 
-firstList.sort();
-secondList.sort();
+// Part One
+function calculateTotalDistance(leftList: number[], rightList: number[]) {
+	// sort the lists
+	leftList.sort((a, b) => a - b);
+	rightList.sort((a, b) => a - b);
 
-const distances = firstList.map((firstNum, index) =>
-	Math.abs(firstNum - secondList[index])
+	// find the distance between items on the two lists
+	const distances = leftList.map((leftNum, index) =>
+		Math.abs(leftNum - rightList[index])
+	);
+	// sum up the distances
+	return distances.reduce((total, distance) => total + distance, 0);
+}
+
+// Part Two
+function calculateSimilarityScore(leftList: number[], rightList: number[]) {
+	const countTable: { number?: number } = {};
+	// initialize items from left list
+	for (const leftNum of leftList) {
+		countTable[leftNum] = 0;
+	}
+	// count the number of times each leftNum appears in rightList
+	for (const rightNum of rightList) {
+		if (countTable[rightNum] >= 0) {
+			countTable[rightNum]++;
+		}
+	}
+	// Calculate a total similarity score by multiplying each left number by
+	// its appearance count in the right list and then adding up.
+	let similarityScore = 0;
+	for (const leftNum of leftList) {
+		similarityScore += leftNum * countTable[leftNum];
+	}
+
+	return similarityScore;
+}
+
+const { leftList, rightList } = setup();
+
+console.log("Total distances: " + calculateTotalDistance(leftList, rightList));
+
+console.log(
+	"Similarity score: " + calculateSimilarityScore(leftList, rightList)
 );
-
-const totalDistance = distances.reduce(
-	(total, distance) => total + distance,
-	0
-);
-
-console.log("Total distances: " + totalDistance);
