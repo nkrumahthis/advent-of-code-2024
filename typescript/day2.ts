bb9114ce570600c5641490ec6ebea1b68ae222be
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-12-03 01:29:36 +0000

    improved day2

--- typescript/day2.ts
+++ typescript/day2.ts
@@ -1,80 +1,65 @@
 import { readFileSync } from "fs";
 
-function setup() {
-	// const data:string = readFileSync("./input/day2.txt").toString();
-    const data = `7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9`
-
-	const lines = data.split("\n");
-
-	const reports: number[][] = [];
-
-	for (const line of lines) {
-		reports.push(line.split(" ").map((report) => Number(report.trim())));
-	}
-
-	return reports;
+function setup(): number[][] {
+    
+//     const TEST_DATA = `7 6 4 2 1
+// 1 2 7 8 9
+// 9 7 6 2 1
+// 1 3 2 4 5
+// 8 6 4 4 1
+// 1 3 6 7 9`
+    // const data = TEST_DATA
+	const data = readFileSync("./input/day2.txt", "utf-8").trim();
+	return data.split("\n").map((line) => line.split(" ").map(Number));
 }
 
 const reports = setup();
 
-function checkSafe(report: number[]): boolean {
-    let prev = report[0];
-    let diffs: number[] = []
-    for (let i = 1; i < report.length; i++) {
-        diffs.push(report[i] - prev)
-        prev = report[i]
-    }
+// part 1
+function checkIsSafe(report: number[]): boolean {
+	const firstDiff = report[1] - report[0];
+	let increasing = firstDiff > 0;
+	let decreasing = firstDiff < 0;
 
-    for (let i = 0; i < report.length; i++) {
-        const level = report[i];
-        console.log([...report.slice(0, i), ...report.slice(i+1, report.length)], report)
-        // const reportWhenSkipped = [...report.slice(0, i-1), ...report.slice(i + 1, report.length)]
-        // console.log("skip", level, report, reportWhenSkipped)
-    }
+	for (let i = 1; i < report.length; i++) {
+		const diff = report[i] - report[i - 1];
+		const absDiff = Math.abs(diff);
 
-    let isIncreasing = diffs[0] > 0 
-    let isDecreasing = diffs[0] < 0
-    let violations = 0
-    for (let i = 0; i < diffs.length; i++) {
-        const diff = diffs[i]
+		if (diff === 0 || absDiff < 1 || absDiff > 3) return false;
+		if ((increasing && diff < 0) || (decreasing && diff > 0)) return false;
+	}
 
-        
-
-        let isViolation = false
-        if ((isIncreasing && diff < 0) || (isDecreasing && diff > 0)){
-            isViolation = true
-            violations++
-        }
-
-        const absDiff = Math.abs(diff)
-        if (absDiff > 3 || absDiff < 1) {
-            isViolation = true
-            violations++
-        }
-
-        // if(isViolation) {
-
-        //     const reportWhenSkipped = [...report.slice(0, index-1), ...report.slice(index + 1)]
-        //     console.log(report, reportWhenSkipped)
-        // }
-
-        if (violations > 1) {
-            return false
-        }
-    }
-    console.log(report, violations)
-
-    return true
+	return true;
 }
 
-let safeCount = 0
+let safeCount = 0;
 for (const report of reports) {
-    if (checkSafe(report)) safeCount++
+	if(checkIsSafe(report)) safeCount++;
 }
 
-console.log(safeCount);
+console.log(`Number of safe reports: ${safeCount}`);
+
+
+// part 2
+function isSafeWithDampener(report: number[]): boolean {
+	if (checkIsSafe(report)) return true;
+
+	for (let i = 0; i < report.length; i++) {
+		if (checkIsSafe(report.slice(0, i).concat(report.slice(i + 1)))) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+let safeCountWithDampener = 0;
+
+for (const report of reports) {
+	if (isSafeWithDampener(report)) {
+		safeCountWithDampener++;
+	}
+}
+
+console.log(`Number of safe reports with dampener: ${safeCountWithDampener}`);
